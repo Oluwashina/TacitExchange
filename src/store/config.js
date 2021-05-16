bc08d90c02c5ef654bffb31c25a5a7d7d21879ff
Oluwashina <shinzbaba@gmail.com>
2021-05-16 23:01:02 +0100

    live url added

--- src/store/config.js
+++ src/store/config.js
@@ -1,2 +1,2 @@
-export const apiUrl = "https://tacit-exchanger.herokuapp.com/";
-// export const apiUrl = "http://134.209.160.224:6000/";
+// export const apiUrl = "https://tacit-exchanger.herokuapp.com/";
+export const apiUrl = "https://exchanger.tacitexchange.com/";
